3a78105b81740a71397326f3f7e4cb82ab39f9c8
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2026-05-03 17:22:10 +0530

    redo.sh has fixed commit in #25

--- issues/issue25/redo.sh
+++ issues/issue25/redo.sh
@@ -1,3 +1,5 @@
+echo "Fetching baseline dictionary from csl-orig..."
+git -C ../../../../sanskrit-lexicon/csl-orig show 34263ba59be878831ab802ee52d17665695bcd5b:v02/ap/ap.txt > temp_ap_0.txt
 echo "Running step 1..."
 python3 step1.py temp_ap_0.txt temp_ap_1.txt temp_log1.tsv
 echo "Running step 2..."
